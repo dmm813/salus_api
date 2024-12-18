8b114a9b109303675a6b298d5db322604a5dd1ce
dmm813 <dmm813@gmail.com>
2024-12-18 09:53:44 -0300

    corrigindo merge

--- Salus_Application/ApplicationUsuario.cs
+++ Salus_Application/ApplicationUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Salus_Core.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Salus_Application
 {
-    internal class ApplicationUsuario
+    public class ApplicationUsuario:IApplication<Usuario>
     {
     }
 }
