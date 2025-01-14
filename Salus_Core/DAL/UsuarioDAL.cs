28014e8d434244aa4644ca6d9a2432dc23d47033
dmm813 <dmm813@gmail.com>
2025-01-14 13:07:25 -0300

    inserir, editar implementados

--- Salus_Core/DAL/UsuarioDAL.cs
+++ Salus_Core/DAL/UsuarioDAL.cs
@@ -1,7 +1,9 @@
 ﻿using Salus_Core.Dominio;
+using Salus_Core.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,8 @@ namespace Salus_Core.DAL
 {
     public  class UsuarioDAL:InterfaceDAL<Usuario>
     {
+        private string _consulta = "";
+        private ConexaoDAL _conexaoDAL = new ConexaoDAL();
         public List<Usuario> SelecionaUsuarioLoginSenha(Usuario user)
         {
             return new List<Usuario>();
@@ -21,14 +25,68 @@ namespace Salus_Core.DAL
 
         public void Inserir(Usuario obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                _consulta = "INSERT INTO [dbo].[Usuario]" +
+                    "       ([codUsuario]," +
+                    "       [login]," +
+                    "       [senha]," +
+                    "       [exclusao]," +
+                    "       [acInserir]," +
+                    "       [acExcluir]," +
+                    "       [acEditar]," +
+                    "       [idNivelAcesso])" +
+                    "VALUES" +
+                    "(" + this.RetornaMaxCodigo() + 1 +
+                      "," + obj.Login +
+                      "," + obj.Senha +
+                      "," + obj.Exclusao +
+                      "," + obj.AcInserir +
+                      "," + obj.AcExcluir +
+                      "," + obj.AcEditar +
+                      "," + obj.IDNivelAcesso+
+                    ")";
+                if (ConexaoDAL.ExecutarConsulta(_consulta))
+                {
+                    ControleLog.InsereLog(1, "USUARIO", "SUCESSO", DateTime.Now);
+                }
+            }
+            catch (Exception ex) 
+            {
+                ControleLog.InsereLog(1, "USUARIO", ex.Message, DateTime.Now);
+                return;
+            }
+
         }
 
         public void Editar(Usuario obj)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                _consulta = "UPDATE [dbo].[Usuario] " +
+                               "SET [login] = " + obj.Login +
+                                  ",[senha] = " + obj.Senha +
+                                  ",[exclusao] = " + obj.Exclusao +
+                                  ",[acInserir] = " + obj.AcInserir +
+                                  ",[acExcluir] = " + obj.AcExcluir +
+                                  ",[acEditar] = " + obj.AcEditar +
+                                  ",[idNivelAcesso] = " + obj.IDNivelAcesso +
+                             "WHERE codUsuario = " + obj.CODUsuario;
+                if (ConexaoDAL.ExecutarConsulta(_consulta))
+                {
+                    ControleLog.InsereLog(2, "USUARIO", "SUCESSO", DateTime.Now);
+                }
+            }
+                
+            catch (Exception ex)
+            {
 
+                ControleLog.InsereLog(2, "USUARIO", ex.Message, DateTime.Now);
+                return;
+            }
+
+        }
         public void Excluir(Usuario obj)
         {
             throw new NotImplementedException();
@@ -36,7 +94,19 @@ namespace Salus_Core.DAL
 
         public List<Usuario> Listar()
         {
-            throw new NotImplementedException();
+            List<Usuario> retorno = new List<Usuario> ();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                ControleLog.InsereLog(2, "USUARIO", ex.Message, DateTime.Now);
+                
+            }
+            return retorno;
+
         }
 
         public List<Usuario> SelecionarPorCodigo(Usuario obj)
