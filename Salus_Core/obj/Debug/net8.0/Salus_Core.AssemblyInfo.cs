e2b9ee7aa41bd52025a051d9a9b1e776428d79d3
dmm813 <dmm813@gmail.com>
2025-01-15 12:05:10 -0300

    reafatorando dalusuario

--- Salus_Core/obj/Debug/net8.0/Salus_Core.AssemblyInfo.cs
+++ Salus_Core/obj/Debug/net8.0/Salus_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Salus_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc833d739e7953cc2ea51791db2b311f0d58eaac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095b629465aad4d423bc0ceb0974c066d60ae803")]
 [assembly: System.Reflection.AssemblyProductAttribute("Salus_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Salus_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
