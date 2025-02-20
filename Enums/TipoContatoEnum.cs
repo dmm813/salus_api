fa4d33e923a676dba561eb21dd52d3b9f6e2c657
dmm813 <dmm813@gmail.com>
2025-02-20 15:50:23 -0300

    SAL-74 criando estrutura de cliente quebrando o clienet e o contato. foi criado duas entidades modelos

---
+++ Enums/TipoContatoEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Salus_2._0.Enums
+{
+    public enum TipoContatoEnum
+    {
+        EMAIL = 1,
+        TELEFONE 
+    }
+}
---
+++ Models/ClienteModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Salus_2._0.Models
+{
+    public class ClienteModel
+    {
+        #region Propriedades
+
+        [Key]
+        public int Id { get; set; }
+        
+        public int Cod { get; set; }
+        
+        public string? Nome { get; set; }
+        
+        public DateTime DataCadastro { get; set; }
+        
+        public string? DocIdentidade { get; set; }
+        
+        public string? CPFCNPJ { get; set; }
+        
+        public string? CEP { get; set; }
+        
+        public string? Rua { get; set; }
+        
+        public string? Numero { get; set; }
+        
+        public string? Complemento { get; set; }
+        
+        public string? Bairro { get; set; }
+        
+        public int IdMunicipio { get; set; }
+        
+        public bool Ativo { get; set; }
+        
+        public bool Exclusao{ get; set;}
+        #endregion
+        public List<ContatoModel>? Contatos { get; set; }
+    }
+}
